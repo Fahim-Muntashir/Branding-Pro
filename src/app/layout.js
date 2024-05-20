bce29ec4aeaaf4755b1552f465eef4f97f4c6db4
FahimMuntashir1 <fahimasdf07@gmail.com>
2024-05-20 23:56:36 +0600

    add

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,9 +4,9 @@ import "rc-drawer/assets/index.css";
 const inter = Inter({ subsets: ["latin"] });
 import "./globals.css";
 export const metadata = {
-  title: "Massive Growth",
+  title: "Branding Proo",
   description:
-    "Welcome to Massive Growth Agency, your premier destination for unparalleled business and personal branding success. Specializing in video editing and website development, we propel your growth through compelling content and powerful online presence.Our services include weekly video production and website maintenance in our Starter Package, while our Pro Package offers monthly video marketing masterpieces, website maintenance sessions, and social media upkeep.Custom solutions tailored to your unique needs are available upon request. Elevate your brand with us today and experience limitless possibilities. Contact us now!",
+    "Welcome to Branding Pro Agency, your go-to for top-tier business and personal branding. We specialize in video editing and website development to boost your growth with compelling content and a strong online presence.    ",
 };
 
 export default function RootLayout({ children }) {
