800b9e6076c43ce77322aceb70183991ac64f3c8
FahimMuntashir1 <fahimasdf07@gmail.com>
2024-06-06 11:35:39 +0600

    adf

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="fj03Ws8y0JkFm60t0h1ng4e04sNtUK_3L5pfDrYUe0o"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
       <script
         type="text/javascript"
