7eda5e6d02f9242adf58524c5d322a82c1131c55
FahimMuntashir1 <fahimasdf07@gmail.com>
2024-08-02 13:13:05 +0600

    a

--- src/Sections/Contact.tsx
+++ src/Sections/Contact.tsx
@@ -180,7 +180,6 @@ const Contact = () => {
                                             name="message"
                                             value={formData.message}
                                             onChange={handleChange}
-                                            rows="6"
                                             placeholder="Your Message"
                                             className="border-stroke dark:border-dark-3 dark:text-dark-6 dark:bg-dark text-body-color focus:border-primary w-full resize-none rounded border py-3 px-[14px] text-base outline-none"
                                         ></textarea>
